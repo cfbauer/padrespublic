b6def2600eb1eee37aa3beafc40ae95392973d24
Chris Bauer <c_bauer2@u.pacific.edu>
2014-05-29 01:37:17 -0500

    updated addthis, added pullquote js, wow js attempt at setting up

--- wordpress/wp-content/themes/padres-public/header.php
+++ wordpress/wp-content/themes/padres-public/header.php
@@ -18,11 +18,13 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type"text/javascript" src="https://ia902500.us.archive.org/22/items/gfycat_hover/gfycat_hover.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/wow.min.js" type="text/javascript"></script>
 
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 
+<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/inc/animate.css">
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 <link rel="shortcut icon" href="<?php bloginfo( 'template_url' ); ?>/favicon.ico" />
 
--- wordpress/wp-content/themes/padres-public/js/scripts.js
+++ wordpress/wp-content/themes/padres-public/js/scripts.js
@@ -30,6 +30,42 @@ $(document).ready(function() {
     $('div.blog-card:odd').addClass('odd');
     $('div.blog-card:even').addClass('even');
 
+    // pull quotes
+    pullquotes = $('span.pullquote'); // Get all pullquotes
+    if (pullquotes.length != 0) { // There are pullquotes
+      pullquotes.each(function(){
+        pullquote = $(this);
+        pullquotecontent = pullquote.text();
+        pullquotecontent = pullquotecontent.replace(/\((.*)\)/gi, ''); // Remove unwanted characters
+        ellipsis = '&#8230;'; // Ellipsis
+        firstchar = pullquotecontent.slice(0,1); // Get first character
+        if (firstchar.toUpperCase() != firstchar) { // First character is not uppercase
+          pullquotecontent = ellipsis + pullquotecontent; // Prepend ellipsis
+        }
+        lastchar = pullquotecontent.slice(-1); // Get last character
+        if ((lastchar != '.') && (lastchar != '?') && (lastchar != '!')) { // Last character is not sentence ending
+          pullquotecontent += ellipsis; // Append ellipsis
+        }
+        classcontent = 'pullquote';
+        if (pullquote.is('.right')) {
+          classcontent += ' right';
+        }
+        pullquote.parent().before('<blockquote class="' + classcontent + '"><p><q>' + pullquotecontent + '</q></p></blockquote>');
+      });
+    }
+
+    // wow entrance animation 
+
+    var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'fadeInRight', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true        // trigger animations on mobile devices (true is default)
+      }
+    );
+    wow.init();
+
 });
 
 
