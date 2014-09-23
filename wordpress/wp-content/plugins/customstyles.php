7131e337089a90cb159502d1a88b659e5583630f
Chris Bauer <c_bauer2@u.pacific.edu>
2014-09-23 01:31:35 -0500

    updated all the things

--- wordpress/wp-content/plugins/customstyles.php
+++ wordpress/wp-content/plugins/customstyles.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Custom Quick Styles
-Plugin URI: http://code.tutsplus.com/tutorials/adding-custom-styles-in-wordpress-tinymce-editor--wp-24980
+Plugin Name: Custom Styles
+Plugin URI: http://www.speckygeek.com
 Description: Add custom styles in your posts and pages content using TinyMCE WYSIWYG editor. The plugin adds a Styles dropdown menu in the visual post editor.
 Based on TinyMCE Kit plug-in for WordPress
 http://plugins.svn.wordpress.org/tinymce-advanced/branches/tinymce-kit/tinymce-kit.php
@@ -15,7 +15,7 @@ function tuts_mcekit_editor_style($url) {
     if ( !empty($url) )
         $url .= ',';
  
-    // Retrieves the plugin directory URL and adds editor stylesheet
+    // Retrieves the plugin directory URL
     // Change the path here if using different directories
     $url .= trailingslashit( plugin_dir_url(__FILE__) ) . '/editor-styles.css';
  
@@ -24,56 +24,52 @@ function tuts_mcekit_editor_style($url) {
  
 /**
  * Add "Styles" drop-down
- */ 
-function tuts_mcekit_editor_buttons($buttons) {
-    array_unshift($buttons, 'styleselect');
+ */
+add_filter( 'mce_buttons_2', 'tuts_mce_editor_buttons' );
+ 
+function tuts_mce_editor_buttons( $buttons ) {
+    array_unshift( $buttons, 'styleselect' );
     return $buttons;
 }
  
-add_filter('mce_buttons_2', 'tuts_mcekit_editor_buttons');
- 
 /**
- * Add "Styles" drop-down content or classes
- */ 
-function tuts_mcekit_editor_settings($settings) {
-    if (!empty($settings['theme_advanced_styles']))
-        $settings['theme_advanced_styles'] .= ';';    
-    else
-        $settings['theme_advanced_styles'] = '';
+ * Add styles/classes to the "Styles" drop-down
+ */
+add_filter( 'tiny_mce_before_init', 'tuts_mce_before_init' );
  
-    /**
-     * Add styles in $classes array.
-     * The format for this setting is "Name to display=class-name;".
-     * More info: http://wiki.moxiecode.com/index.php/TinyMCE:Configuration/theme_advanced_styles
-     *
-     * To be allow translation of the class names, these can be set in a PHP array (to keep them
-     * readable) and then converted to TinyMCE's format. You will need to replace 'textdomain' with
-     * your theme's textdomain.
-     */
-    $classes = array(
-        __('Warning','textdomain') => 'warning',
-        __('Notice','textdomain') => 'notice',
-        __('Download','textdomain') => 'download',
-        __('Testimonial','textdomain') => 'testimonial box',
+function tuts_mce_before_init( $settings ) {
+ 
+    $style_formats = array(
+        array(
+            'title' => 'Pullquote',
+            'wrapper' => 'blockquote',
+            'classes' => 'download'
+            ),
+        array(
+            'title' => 'Testimonial',
+            'selector' => 'p',
+            'classes' => 'testimonial',
+        )   
     );
  
-    $class_settings = '';
-    foreach ( $classes as $name => $value )
-        $class_settings .= "{$name}={$value};";
+    $settings['style_formats'] = json_encode( $style_formats );
  
-    $settings['theme_advanced_styles'] .= trim($class_settings, '; ');
     return $settings;
-} 
  
-add_filter('tiny_mce_before_init', 'tuts_mcekit_editor_settings');
+}
+ 
+/* Learn TinyMCE style format options at http://www.tinymce.com/wiki.php/Configuration:formats */
  
 /*
  * Add custom stylesheet to the website front-end with hook 'wp_enqueue_scripts'
- * Enqueue the custom stylesheet in the front-end
  */
 add_action('wp_enqueue_scripts', 'tuts_mcekit_editor_enqueue');
+ 
+/*
+ * Enqueue stylesheet, if it exists.
+ */
 function tuts_mcekit_editor_enqueue() {
-  $StyleUrl = plugin_dir_url(__FILE__).'editor-styles.css';
+  $StyleUrl = plugin_dir_url(__FILE__).'editor-styles.css'; // Customstyle.css is relative to the current file
   wp_enqueue_style( 'myCustomStyles', $StyleUrl );
 }
 ?>
