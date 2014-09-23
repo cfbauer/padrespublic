8803f18ef43824e502821bd953e681479eab70e8
Chris Bauer <c_bauer2@u.pacific.edu>
2014-09-23 01:56:08 -0500

    updated a couple plugins, fixed pullquote displaying twice issue with editor css displaying on the site

--- wordpress/wp-content/themes/padres-public/js/scripts.js
+++ wordpress/wp-content/themes/padres-public/js/scripts.js
@@ -1,36 +1,5 @@
-wow = function() {
-}
-
 
 $(document).ready(function() {
-    var effect = "tossing";
-
-    function delayedEffect() {
-      $('.box').addClass(effect);
-    }
-    function delayedRemoveBlock() {
-      $('.box').removeClass("block");    
-    }
-
-    $('.click').click(function() {
-      if (!$('.box').hasClass(effect)) {
-         $('.box').fadeIn();
-    		 $('.box').addClass("bigEntrance");
-         $('.box').addClass("block");
-        
-         setTimeout(delayedEffect, 2200);
-          
-      } else {
-        
-      $('.box').fadeOut();   
-      $('.box').removeClass("bigEntrance");
-      $('.box').removeClass(effect);
-      
-      setTimeout(delayedRemoveBlock, 2200);
-      
-      }
-    });
-
     $('div.blog-card:odd').addClass('odd');
     $('div.blog-card:even').addClass('even');
 
